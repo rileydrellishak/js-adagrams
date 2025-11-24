082eeff3b21ceca81a9e25fc2882204eec4262ba
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-23 20:54:22 -0500

    refactors overall datastructure of letters, their frequencies, and their values so I could scope the data structure to only the functions that need it. this commit updates waves 1 and 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,29 +1,33 @@
-
-
-const LETTERS = {
-  A: {freq: 9, value: 1}, B: {freq: 2, value: 3}, C: {freq: 2, value: 3},
-  D: {freq: 4, value: 2}, E: {freq: 12, value: 1}, F: {freq: 2, value: 4},
-  G: {freq: 3, value: 2}, H: {freq: 2, value: 4}, I: {freq: 9, value: 1},
-  J: {freq: 1, value: 8}, K: {freq: 1, value: 5}, L: {freq: 4, value: 1},
-  M: {freq: 2, value: 3}, N: {freq: 6, value: 1}, O: {freq: 8, value: 1},
-  P: {freq: 2, value: 3}, Q: {freq: 1, value: 10}, R: {freq: 6, value: 1},
-  S: {freq: 4, value: 1}, T: {freq: 6, value: 1}, U: {freq: 4, value: 1},
-  V: {freq: 2, value: 4}, W: {freq: 2, value: 4}, X: {freq: 1, value: 8},
-  Y: {freq: 2, value: 4}, Z: {freq: 1, value: 10}
-};
-
 const buildLetterPool = () => {
+  const LETTER_FREQUENCIES = {
+    A: 9, B: 2, C: 2, D: 4,
+    E: 12, F: 2, G: 3, H: 2,
+    I: 9, J: 1, K: 1, L: 4,
+    M: 2, N: 6, O: 8, P: 2,
+    Q: 1, R: 6, S: 4, T: 6,
+    U: 4, V: 2, W: 2, X: 1,
+    Y: 2, Z: 1
+  };
   const letterPool = [];
-  for (const letter of Object.keys(LETTERS)) {
-    for (let i = 0; i < LETTERS[letter].freq; i ++) {
+  for (const letter of Object.keys(LETTER_FREQUENCIES)) {
+    for (let i = 0; i < LETTER_FREQUENCIES[letter]; i ++) {
       letterPool.push(letter);
     }
   } return letterPool;
 };
 
 const freqCheck = (tile, hand) => {
+  const LETTER_FREQUENCIES = {
+    A: 9, B: 2, C: 2, D: 4,
+    E: 12, F: 2, G: 3, H: 2,
+    I: 9, J: 1, K: 1, L: 4,
+    M: 2, N: 6, O: 8, P: 2,
+    Q: 1, R: 6, S: 4, T: 6,
+    U: 4, V: 2, W: 2, X: 1,
+    Y: 2, Z: 1
+  };
   const countOfLetterInHand = hand.filter((letter) => letter === tile);
-  if (countOfLetterInHand.length < LETTERS[tile].freq) {
+  if (countOfLetterInHand.length < LETTER_FREQUENCIES[tile]) {
     return true;
   } return false;
 };
@@ -69,7 +73,7 @@ export const scoreWord = (word) => {
   const wordUpper = word.toUpperCase();
   let score = 0;
   for (let char of wordUpper) {
-    score += LETTERS[char].value;
+    score += LETTER_FREQUENCIES[char].value;
   } if (wordUpper.length >= 7) {
     score += 8;
   } return score;
