052e660e121504c26c5cec7c0b0b08a67b79a97a
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-23 21:01:08 -0500

    scopes letter scores data structure to the scoreWord function

--- src/adagrams.js
+++ src/adagrams.js
@@ -67,14 +67,21 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
+  const LETTER_SCORES = {
+    A: 1, E: 1, I: 1, O: 1, U: 1,
+    L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5, J: 8, X: 8, Q: 10, Z: 10
+  };
   if (word.trim() === '') {
     return 0;
   }
-  const wordUpper = word.toUpperCase();
   let score = 0;
-  for (let char of wordUpper) {
-    score += LETTER_FREQUENCIES[char].value;
-  } if (wordUpper.length >= 7) {
+  for (let char of word.toUpperCase()) {
+    score += LETTER_SCORES[char];
+  } if (word.length >= 7) {
     score += 8;
   } return score;
 };
